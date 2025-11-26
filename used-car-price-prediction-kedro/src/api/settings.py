eb976d1c89e92555ec155220f31dca26ad4ab611
Adam Bartkiewicz <s27000@pjwstk.edu.pl>
2025-11-26 02:20:33 +0100

    Update: added API_URL to settings.py and .env.example

--- used-car-price-prediction-kedro/src/api/settings.py
+++ used-car-price-prediction-kedro/src/api/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     MODEL_PATH: str | None = None
     WANDB_API_KEY: str | None = None
+    API_URL: str | None = None
     DATABASE_URL: str = "sqlite:///./data/08_reporting/api_predictions.db"
 
     model_config = SettingsConfigDict(env_file=".env")
